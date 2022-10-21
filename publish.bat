8770da10e9d6e9868299622e52bcedddc4af858f
konstantin-ebeling <115927484+konstantin-ebeling@users.noreply.github.com>
2022-10-21 03:26:29 +0200

    fix script

--- publish.bat
+++ publish.bat
@@ -1,13 +1,12 @@
-call ./build.bat
-
-git stash
-
 git branch -D web
 git checkout --orphan web
+rmdir out /S /Q
+call ./build.bat
+rmdir docs /S /Q
 ren out docs
+git reset
 git add docs/*
 git commit -m"update web"
 git push -u origin web --force
 
-git checkout main
-git stash pop
+git checkout main -f
