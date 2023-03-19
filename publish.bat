4123af094b5438eeef9627e6786288f03514c999
konstantin-ebeling <115927484+konstantin-ebeling@users.noreply.github.com>
2023-03-19 20:35:36 +0100

    fix publish.bat

--- publish.bat
+++ publish.bat
@@ -1,11 +1,9 @@
+call ./build.bat
 git branch -D web
 git checkout --orphan web
-rmdir out /S /Q
-call ./build.bat
 rmdir docs /S /Q
 ren out docs
-git reset
-git add docs/*
+git add .
 git commit -m"update web"
 git push -u origin web --force
 
